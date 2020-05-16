d54518936ecd5977a5e76a6707b482df49e12a24
Daniel Falk <daniel@da-robotteknik.se>
2020-05-16 16:12:45 +0200

    Make package installable
    Annotation and traing is done on host (in cpython) so make this package
    installable using pip.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='camera-analytics',
+    description='Neural network based visual door state prediction',
+    long_description='''
+    AXIS network camera application for visual analytics
+    to classify the state of a door (open/closed)''',
+    version="0.0",
+    packages=['camlytics'],
+    url='https://github.com/daniel-falk/camera-analytics',
+    author='Daniel Falk',
+    author_email='daniel@da-robotteknik.se',
+    license='MIT',
+    install_requires=required,
+    entry_points={
+        'console_scripts': [
+            'annotate=camlytics.annotation.annotate:do_annotate'
+        ],
+    }
+)
